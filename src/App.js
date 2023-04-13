896f635ea23a61621c79117cd4eb69121480494b
claudioescobar <escobarc697@gmail.com>
2023-04-13 17:07:17 -0300

    ItemCount

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemsListContainer from './components/ItemsListContainer/ItemsListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <div className="App">
         <NavBar/>
         <ItemsListContainer greeting={'Bienvenidos'}/>
+        <ItemCount initial={1} stock= {10} onAdd={(quantity)=> console.log('Cantidad agregada' , quantity)}/>
     </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,19 @@
 import CartWidget from "../CartWidget/CartWidget"
+import './NavBar.css'
 
 const NavBar = ()=>{
     return (
-        <nav>
+        <nav className="navBar">
             <h1>"PeMoa"</h1>
-            <h3>Tienda de ropa </h3>
             <div>
-                <button>Remeras</button>
-                <button>Pantalones</button>
-                <button>Buzos</button>
+                <h3>Tienda de ropa </h3>
+                <div className="option">
+                    <button>Remeras</button>
+                    <button>Pantalones</button>
+                    <button>Buzos</button>
+                </div>
+                <CartWidget/>
             </div>
-            <CartWidget/>
         </nav>
         
     )
